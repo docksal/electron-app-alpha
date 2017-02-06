fae012efeb7029aacc05dfaac2d889158591ce79
Alexei Chekulaev <alexei.chekulaev@ffwagency.com>
2017-02-05 22:02:35 -0500

    Improve appLog

--- app/appLog.js
+++ app/appLog.js
@@ -9,18 +9,45 @@ const LOGFILE = '/tmp/docksal-ui.log';
 const ERRFILE = '/tmp/docksal-ui.err';
 
 let debugEnabled = false;
+let errorLevel = 0;
 
+const DEBUG = 0;
+const INFO = 1;
+const WARN = 2;
+const ERROR = 3;
+
+exports.log = (level, msg) => {
+  if (!debugEnabled || errorLevel > level) return;
+  let fileName = "";
+  let prefix = '';
+  (level == INFO) ? prefix = '<INFO> ' : 0;
+  (level == WARN) ? prefix = '<WARN> ' : 0;
+  (level == ERROR) ? prefix = '<ERROR> ' : 0;
+  let line = fileName + prefix + msg + (msg[msg.length-1] == "\n" ? "" : "\n");
+
+  if (level < 3) {
+    fs.writeFileSync(LOGFILE, line, {flag: 'a'});
+  } else {
+    fs.writeFileSync(ERRFILE, line, {flag: 'a'});
+  }
+
+  console.log(line.trim());
+};
 
 exports.debug = (msg) => {
-  if (!debugEnabled) return;
-  let fileName = "";
-  fs.writeFileSync(LOGFILE, fileName + msg + (msg[msg.length-1] == "\n" ? "" : "\n"), {flag: 'a'});
+  exports.log(DEBUG, msg);
+};
+
+exports.info = (msg) => {
+  exports.log(INFO, msg);
+};
+
+exports.warn = (msg) => {
+  exports.log(WARN, msg);
 };
 
 exports.error = (msg) => {
-  if (!debugEnabled) return;
-  let fileName = "";
-  fs.writeFileSync(ERRFILE, fileName + msg + (msg[msg.length-1] == "\n" ? "" : "\n"), {flag: 'a'});
+  exports.log(ERROR, msg);
 };
 
 exports.start = () => {
@@ -29,6 +56,10 @@ exports.start = () => {
 
 exports.stop = () => {
   debugEnabled = false;
+};
+
+exports.setErrorLevel = (level) => {
+  errorLevel = level;
 };
 
 let logStart = 'Log start ' + (new Date()).toString() + "\n-------------------------\n";

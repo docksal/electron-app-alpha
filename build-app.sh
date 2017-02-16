5a443448befc3f1eaf8d7e575a068274fb0c8651
Alexei Chekulaev <alexei.chekulaev@ffwagency.com>
2017-02-16 16:34:13 -0500

    build-app improvements

--- build-app.sh
+++ build-app.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
+## PLATFORM:
+##  use win32 for Windows
+##  darwin for macOS
+##  linux for Linux
+##  leave empty to build app for platform you're running at
+
 if [[ "$1" != "" ]]; then
 	PLATFORM="--platform=$1"
 fi
+
+[[ "$1" == "" ]] &&
+	echo "Building Docksal UI for current platform" ||
+	echo "Building app for $1 platform"
+echo "Press Enter to continue..."
+read -p ''
 
 npm install &&
 	electron-packager . \
